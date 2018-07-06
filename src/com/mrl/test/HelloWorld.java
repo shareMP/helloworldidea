8d8f9ed68207fc6e942b2e0f776992cdf287c76e
companycomputer <544520418@qq.com>
2018-07-06 12:35:22 +0800

    commit

--- src/com/mrl/test/HelloWorld.java
+++ src/com/mrl/test/HelloWorld.java
@@ -5,6 +5,7 @@ package com.mrl.test;
  */
 public class HelloWorld {
     public static void main(String[] args){
+        System.out.println("-----");
         System.out.println("hello,world!----]]]]");
 
     }
