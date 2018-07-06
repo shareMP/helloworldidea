84bbe1ff77ce3d68ae81d7df9989b82b134d69ee
companycomputer <544520418@qq.com>
2018-07-06 12:51:28 +0800

    commit1

--- src/com/mrl/test/HelloWorld.java
+++ src/com/mrl/test/HelloWorld.java
@@ -4,13 +4,13 @@ package com.mrl.test;
  *
  */
 public class HelloWorld {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("-----");
         System.out.println("hello,world!----]]]]");
         System.out.println("----sssssss-");
-          System.out.println("----sssssss-");
-          System.out.println("----sssssss-");
-          System.out.println("----sssssss-");  
+        System.out.println("----sssssss-");
+        System.out.println("----sssssss-");
+        System.out.println("----sssssss-");
         System.out.println("----sssssss-");
     }
 }
